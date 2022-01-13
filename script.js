11b23285e8b5d99ec0326aae6819438527f56c01
z1youRA <374671110@qq.com>
2022-01-14 00:23:03 +0800

    add methods to examine status and winner

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const Player = (number, name, type) => {
     const placeChecker = (position) => {
         if(Gameboard.gameboard[position] === ''){ // check if the block is empty.
             Gameboard.gameboard[position] = type;
+            game.checkStatus(position);
             game.nextTurn();
         }
         displayController.refreshBoard()
@@ -32,10 +33,10 @@ const displayController = (() => {
             block.setAttribute('data-key', `${index}`); // locate the position of checkers.
             block.addEventListener('click', (e) => {
                 if (game.getTurn() % 2 === 1) {
-                    player1.placeChecker(e.target.getAttribute('data-key'));
+                    player1.placeChecker(parseInt(e.target.getAttribute('data-key')));
                 }
                 else if(game.getTurn() % 2 === 0) {
-                    player2.placeChecker(e.target.getAttribute('data-key'));
+                    player2.placeChecker(parseInt(e.target.getAttribute('data-key')));
                 }
             })
             block.textContent = piece;
@@ -48,14 +49,59 @@ const displayController = (() => {
 
 const game = (() => {
     let turn = 1;
+    const _checkRow = (row) => {
+        for(let i = row * 3; i < (row + 1) * 3 - 1; i++) {
+            if(Gameboard.gameboard[i] == '') {
+                return 0;
+            }
+            if(Gameboard.gameboard[i] !== Gameboard.gameboard[i + 1])
+                return 0;
+        }
+        return 1;
+    }
+
+    const _checkCol = (column) => {
+        for(let i = column; i <= 3 + column; i = i + 3) {
+            if(Gameboard.gameboard[i] == '') {
+                return 0;
+            }
+            if(Gameboard.gameboard[i] !== Gameboard.gameboard[i + 3])
+                return 0;
+        }
+        return 1;
+    }
+
+    const _checkDia = (position) => {
+        if(position % 2 == 0) {
+            if(position == 0 || position == 4 || position == 8) {
+                if(Gameboard.gameboard[0] !== '' && Gameboard.gameboard[0] == Gameboard.gameboard[4] && Gameboard.gameboard[4] == Gameboard.gameboard[8]){
+                    return 1;
+                }
+            }
+            else if(position == 2 || position == 4 || position == 6) {
+                if(Gameboard.gameboard[2] !== '' && Gameboard.gameboard[2] == Gameboard.gameboard[4] && Gameboard.gameboard[4] == Gameboard.gameboard[6]){
+                    return 1;
+                }
+            }
+        }
+        return 0; // Without this _checkDia will return '0' instead of number 0;
+    }
     const getTurn = () => turn;
     const nextTurn = () => {
         turn++;
     }
-    const checkStatus = () => {
-
+    const checkStatus = (position) => {
+        const row = Math.floor(position / 3);
+        const column = position % 3;
+        let flag = 0;
+        flag += _checkRow(row);
+        flag += _checkCol(column);
+        flag += _checkDia(position);
+        if(flag !== 0) {
+            alert('win')
+        }
     }
-    return {getTurn, nextTurn};
+    return {getTurn, nextTurn, checkStatus};
 })();
 
 
