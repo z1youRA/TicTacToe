093f36ffc8b9d3003d84b58419d76b67132114aa
z1youRA <374671110@qq.com>
2022-01-16 17:57:52 +0800

    add popup window ajust ui design

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const Player = (number, name, type) => {
         if(Gameboard.gameboard[position] === ''){ // check if the block is empty.
             Gameboard.gameboard[position] = type;
             displayController.refreshBoard();
-            game.checkStatus(position);
+            game.checkStatus(position, number);
             game.nextTurn();
         }
     }
@@ -20,41 +20,12 @@ const Player = (number, name, type) => {
 
 
 
-const displayController = (() => {
-    const container = document.querySelector('.container');
-    const refreshBoard = () => {
-        while(container.hasChildNodes()) {
-            container.removeChild(container.firstChild);
-        }
-        Gameboard.gameboard.forEach((piece, index) => {
-            const block = document.createElement('div');
-            block.classList.add("piece");
-            block.setAttribute('data-key', `${index}`); // locate the position of checkers.
-            block.addEventListener('click', (e) => {
-                if (game.getTurn() % 2 === 1) {
-                    game.player1.placeChecker(parseInt(e.target.getAttribute('data-key')));
-                }
-                else if(game.getTurn() % 2 === 0) {
-                    game.player2.placeChecker(parseInt(e.target.getAttribute('data-key')));
-                }
-            })
-            block.textContent = piece;
-            container.appendChild(block);
-        })
-    }
-    return {refreshBoard};
-})();
+
 
 const game = (() => {
     let turn = 1;
-    let p1Name = document.getElementById('P1-name').value;
-    let p2Name = document.getElementById('P2-name').value;
-    if(p1Name === '')
-        p1Name = 'Player 1';
-    if(p2Name === '')
-        p2Name = 'Player 2';
-    const player1 = Player(1, p1Name, 'X');
-    const player2 = Player(2, p2Name, "O");
+    let player1 = Player(1, 'Player 1', 'X');
+    let player2 = Player(2, 'Player 2', "O");
 
     const _checkRow = (row) => {
         for(let i = row * 3; i < (row + 1) * 3 - 1; i++) {
@@ -93,30 +64,106 @@ const game = (() => {
         }
         return 0; // Without this _checkDia will return '0' instead of number 0;
     }
-    const _start = () => {
+    const start = () => {
+        let p1Name = document.getElementById('P1-name').value;
+        let p2Name = document.getElementById('P2-name').value;
+        if(p1Name === '')
+            p1Name = 'Player 1';
+        if(p2Name === '')
+            p2Name = 'Player 2';
+        game.player1.name = p1Name;
+        game.player2.name = p2Name;
         displayController.refreshBoard();
     }
-    const startButton = document.querySelector('.start');
-    startButton.addEventListener('click', _start);
+
     const getTurn = () => turn;
+
     const nextTurn = () => {
         turn++;
     }
-    const checkStatus = (position) => {
+
+    const setTurn = (num) => {
+        turn = parseInt(num);
+    }
+
+    const checkStatus = (position, number) => {
         const row = Math.floor(position / 3);
         const column = position % 3;
         let flag = 0;
+        let winner = game.player1;
+        if(number == 2) 
+            winner = game.player2;
         flag += _checkRow(row);
         flag += _checkCol(column);
         flag += _checkDia(position);
         if(flag !== 0) {
-            alert('win');
+            displayController.popMessage(`${player1.name} WON!!!`);
         }
         if(game.getTurn() === 9) {
-            alert('tie');
+            displayController.popMessage('TIE');
         }
     }
-    return {getTurn, nextTurn, checkStatus, player1, player2};
+
+    return {getTurn, nextTurn, setTurn,  checkStatus, start , player1, player2};
 })();
 
+const displayController = (() => {
+    const startButton = document.querySelector('.start');
+    const popupBox = document.querySelector('.popup');
+    const popupText = document.querySelector('.popup .text');
+    const overlay = document.getElementById('overlay');
+    const closeButton = document.querySelector('.close-button');
+    const restartButton = document.querySelector('.restart');
+    const container = document.querySelector('.container');
+
+    closeButton.addEventListener('click', ()=>{
+        popupBox.classList.remove('active');
+        overlay.classList.remove('active');
+    })
+
+    startButton.addEventListener('click', game.start);
+    
+    restartButton.addEventListener('click', () => {
+        popupBox.classList.remove('active');
+        overlay.classList.remove('active');
+        displayController.cleanBoard();
+        game.start();
+    });
+
+    const popMessage = (str) => {
+        popupText.textContent = str;
+        popupBox.classList.add('active');
+        overlay.classList.add('active');
+    }
+
+    const cleanBoard = () => {
+        for (let i = 0; i < 9; i++) {
+            Gameboard.gameboard[i] = '';
+        }
+        game.setTurn(1);
+        refreshBoard();
+    }
+
+    const refreshBoard = () => {
+        while(container.hasChildNodes()) {
+            container.removeChild(container.firstChild);
+        }
+        Gameboard.gameboard.forEach((piece, index) => {
+            const block = document.createElement('div');
+            block.classList.add("piece");
+            block.setAttribute('data-key', `${index}`); // locate the position of checkers.
+            block.addEventListener('click', (e) => {
+                if (game.getTurn() % 2 === 1) {
+                    game.player1.placeChecker(parseInt(e.target.getAttribute('data-key')));
+                }
+                else if(game.getTurn() % 2 === 0) {
+                    game.player2.placeChecker(parseInt(e.target.getAttribute('data-key')));
+                }
+            })
+            block.textContent = piece;
+            container.appendChild(block);
+        })
+    }
+    return {refreshBoard, cleanBoard, popMessage};
+})();
 
